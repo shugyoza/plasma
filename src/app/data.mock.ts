05d31a014ad793363524812d92705140fb56fba3
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-08-15 10:45:25 -0700

    adding data.mock.ts with five items

---
+++ src/app/data.mock.ts
@@ -0,0 +1,34 @@
+import { Item } from './item.model';
+
+export const items: Item[] = [
+    {
+        id: '1',
+        title: 'Item 1',
+        description: 'This is description for Item 1',
+        price: 10011
+    },
+    {
+        id: '2',
+        title: 'Item 2',
+        description: 'This is description for Item 2',
+        price: 20022
+    },
+    {
+        id: '3',
+        title: 'Item 3',
+        description: 'This is description for Item 3',
+        price: 30033
+    },
+    {
+        id: '4',
+        title: 'Item 4',
+        description: 'This is description for Item 4',
+        price: 44044
+    },
+    {
+        id: '5',
+        title: 'Item 5',
+        description: 'This is description for Item 5',
+        price: 55055
+    }
+]
