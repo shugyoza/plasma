e94c46cecbdc29e609b5220b0ddbbfe8223e24cb
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-08-15 11:03:37 -0700

    added path items, checkout, and home redirectTo in the app-routing.module for routing and they works.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListDisplayComponent } from './list-display/list-display.component';
+import { ListCheckoutComponent } from './list-checkout/list-checkout.component';
+
+const routes: Routes = [
+  {
+    path: 'items',
+    component: ListDisplayComponent
+  },
+  {
+    path: 'checkout',
+    component: ListCheckoutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/items',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
