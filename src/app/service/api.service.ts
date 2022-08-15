7f044b1449aa78ec664ec57425f9b62a0d7b446a
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-08-15 10:58:49 -0700

    adding replaySubject to items in api.service, and delete default app.component.html boilerplate codes except the router-outlet.

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+
+import { Item } from '../model/item.model';
+import { items } from '../model/data.mock';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  items = new ReplaySubject();
+
   constructor() { }
+
+  getItems(): void {
+    this.items.next(items);
+  }
+
 }
