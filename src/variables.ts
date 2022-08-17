091f3897510947f90bfd8285acdb0980bbfb69df
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-08-17 01:08:20 -0700

    modify endpoint to reflect current heroku endpoint.

--- src/variables.ts
+++ src/variables.ts
@@ -3,5 +3,5 @@ export const variables = {
         emailAddress: null, // input your email address here
         emailContent: null // input content you want here for testing
     },
-    endPoint: "http://localhost:3000/sendmail"
+    endPoint: "https://ngplasma.herokuapp.com/sendmail" // "http://localhost:3000/sendmail"
 }
