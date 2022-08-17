8274fe097e597b165c96d50d106094bfcef4dafe
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-08-17 01:17:31 -0700

    modify endpoint to express-nodemailer.herokuapp...

--- src/variables.ts
+++ src/variables.ts
@@ -3,5 +3,5 @@ export const variables = {
         emailAddress: null, // input your email address here
         emailContent: null // input content you want here for testing
     },
-    endPoint: "https://ngplasma.herokuapp.com/sendmail" // "http://localhost:3000/sendmail"
+    endPoint: "https://express-nodemailer.herokuapp.com/sendmail" // "http://localhost:3000/sendmail"
 }
